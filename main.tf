50c988d94c930cdc12c7f396106e232572d05cba
Tom Coyle <guitardude0825@yahoo.com>
2024-02-07 16:43:04 -0600

    Kill All

--- main.tf
+++ main.tf
@@ -28,7 +28,7 @@ provider "aws" {
 provider "azurerm" {
   features {}
 }
-
+/*
 resource "aws_s3_bucket" "tom_bucket" {
   bucket        = "tcoyleawsbucket123423453"
   force_destroy = true
@@ -66,3 +66,4 @@ resource "azurerm_virtual_network" "tc-vnet" {
 output "ec2_public_ip" {
   value = aws_instance.Terraform.public_ip
 }
+*/
