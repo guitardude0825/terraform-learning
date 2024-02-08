509938ad57d61d31bc37fb1ef65c1d20f8c832b8
Tom Coyle <guitardude0825@yahoo.com>
2024-02-08 10:23:16 -0600

    Test Terraform Show

--- main.tf
+++ main.tf
@@ -28,7 +28,7 @@ provider "aws" {
 provider "azurerm" {
   features {}
 }
-/*
+
 resource "aws_s3_bucket" "tom_bucket" {
   bucket        = "tcoyleawsbucket123423453"
   force_destroy = true
@@ -73,4 +73,3 @@ resource "azurerm_subnet" "tc-subnet-1" {
 output "ec2_public_ip" {
   value = aws_instance.Terraform.public_ip
 }
-*/
